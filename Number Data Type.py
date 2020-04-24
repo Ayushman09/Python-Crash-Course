0da8529c867a67d270dbc02a41c4af67988d7890
Ayushman Chakravarty <ayushmanchak@gmail.com>
2020-04-25 01:12:09 +0530

    Add files via upload

---
+++ Number Data Type.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Data types
+#     * Numbers/Numeric 
+#           *int (007,-7,10,-10) the range is(2147483647,-2147483648) 
+#           *long (-8656367377) 
+#           *float (0.07,0.007, 7.00007 , -0.007, 0e787.0) 
+#           *complex ( 2 + 6j )
+
+# 
+# ### Numbers
+
+# In[1]:
+
+
+1 + 1
+
+
+# In[2]:
+
+
+2 * 6
+
+
+# In[3]:
+
+
+1.2 + 5
+
+
+# In[4]:
+
+
+3.0 * 3.0
+
+
+# In[9]:
+
+
+3.0 * 3
+
+
+# In[12]:
+
+
+3 ** 2 #'**' is known as Exponent operator
+
+
+# In[13]:
+
+
+3 ** 3
+
+
+# In[6]:
+
+
+9/3
+
+
+# In[7]:
+
+
+9.0/3
+
+
+# In[14]:
+
+
+9.0//3 # '//' is floor division - it returns the quotient/answer of the division by removing decimal part
+
+
+# In[15]:
+
+
+10/3
+
+
+# In[16]:
+
+
+10//3  #the use of floor operator '//'
+
+
+# ### I have done only a small set of operations with numbers
+# ### Write more and Ask the doubts!
